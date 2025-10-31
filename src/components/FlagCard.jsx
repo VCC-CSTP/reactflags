a330f093feb9835f88505d58e572cfaa721da1ec
Spuninst <domainjunkie@gmail.com>
2025-10-31 13:24:21 -0700

    completed flags layout and learn more button. working on detail page now

--- src/components/FlagCard.jsx
+++ src/components/FlagCard.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 export const FlagCard = (country) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
+  
       <div className="shadow-md rounded-lg overflow-hidden hover:shadow-lg transition">
         <img
-          src={country.flags.png}
-          alt={country.name.common}
+          src={country.country.flags.png}
+          alt={country.country.name.common}
           className="w-full h-40 object-cover"
         />
 
         <div className="flex justify-between items-center p-4">
-          <span className="font-medium">{country.name.common}</span>
+          <span className="font-medium">{country.country.name.common}</span>
           <Link
-            to={`/detail/${country.name.common}`}
+            to={`/detail/${country.country.name.common}`}
             className="bg-black text-white px-3 py-1 rounded hover:bg-gray-800"
           >
             Learn More
           </Link>
         </div>
       </div>
-    </div>
   );
-}
+};
--- src/components/FlagGrid.jsx
+++ src/components/FlagGrid.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { FlagCard } from "./FlagCard";
 
-const API_URL =
+const API_URL_One =
   "https://restcountries.com/v3.1/name/canada?fields=name,region,subregion,flags";
 
+const API_URL =
+  "https://restcountries.com/v3.1/independent?status=true&fields=name,region,subregion,flags";
+
 export const FlagGrid = () => {
-  const [country, setCountry] = useState(null); // ← Changed to single object
+  const [countries, setCountries] = useState([]); // ← Changed to single object
 
   useEffect(() => {
     getFlagsData();
@@ -15,21 +18,23 @@ export const FlagGrid = () => {
   const getFlagsData = async () => {
     const data = await fetch(API_URL);
     const jsonData = await data.json();
-    console.log(jsonData[0]);
-    setCountry(jsonData[0]); // ← Store first item, not the array
+    //console.log(jsonData);
+    setCountries(jsonData);
   };
 
   // Add loading check
-  if (!country) {
+  if (!countries) {
     return <div className="p-4">Loading...</div>;
   }
 
   return (
     <div className="relative overflow-x-auto">
-      <h1 className="text-4xl font-bold underline">Flags</h1>
-      {country?.map((cntry) => (
-        <FlagCard cntry={cntry} />
-      ))}
+      <h1 className="text-4xl font-bold no-underline p-6">World Flags</h1>
+        <div className="grid grid-cols-4 gap-4">
+            {countries.map((country, index) => (
+                <FlagCard key={index} country={country} />
+            ))}
+        </div>
     </div>
   );
 };
